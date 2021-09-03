5a8e9387f35ddf555298ce59e0d59451613d6d11
Evgeny <gene_v@bk.ru>
2021-09-04 01:14:06 +0300

    initial

---
+++ src/main/java/com/vasche/jenkinsappaxample/JenkinsAppAxampleApplication.java
@@ -0,0 +1,13 @@
+package com.vasche.jenkinsappaxample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAppAxampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAppAxampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasche/jenkinsappaxample/JenkinsAppAxampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasche.jenkinsappaxample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAppAxampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
